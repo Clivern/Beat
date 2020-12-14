672e4e61fc56954c63081578084fd724d439f696
Clivern <hello@clivern.com>
2020-12-14 16:29:47 +0100

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+### Beat
+
+Beat is a command line tool to do fare estimation for a big set of rides.
+
+`Beat` has the following features:
+
+- ..
+
+## Documentation
+
+```
+$ cd $GOPATH
+$ mkdir -p src/bitbucket.org/clivern
+```
+
+
+## Security Issues
+
+If you discover a security vulnerability within Beat, please send an email to [hello@clivern.com](mailto:hello@clivern.com)
+
+
+## Contributing
+
+We are an open source, community-driven project so please feel free to join us. see the [contributing guidelines](CONTRIBUTING.md) for more details.
+
+
+## License
+
+© 2020, Clivern. Released under [MIT License](https://opensource.org/licenses/mit-license.php).
+
+**Beat** is authored and maintained by [@clivern](http://bitbucket.org/clivern).
---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+// Copyright 2020 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"bitbucket.org/clivern/beat/core/util"
+
+	"github.com/spf13/cobra"
+)
+
+// Verbose var
+var Verbose bool
+
+// Config var
+var Config string
+
+var rootCmd = &cobra.Command{
+	Use: "beat",
+	Short: `A Command Line Tool to do Fare Estimation for a big set of rides.
+
+If you have any suggestions, bug reports, or annoyances please report
+them to our issue tracker at <https://bitbucket.org/clivern/beat>`,
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().StringVarP(
+		&Config,
+		"config",
+		"c",
+		"",
+		"config file",
+	)
+}
+
+// Execute runs cmd tool
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
